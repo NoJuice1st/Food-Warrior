4724b5d8e9d0ab84c6c1a6d9b9ccd535656a077d
NoJuice1st <110340406+NoJuice1st@users.noreply.github.com>
2024-02-04 14:53:24 +0200

    Added Bombs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FruitSpawner : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
+    public GameObject bomb;
     public GameObject fruit;
-    public float spawnRate;
+    public float spawnRate = 1f;
+    public float bombChance = 20f;
 
-    void SpawnFruit()
+    void Spawn()
     {
+        var prefab = Random.Range(0, 100) < 100 - bombChance ? fruit : bomb;
+
         var posX = Random.Range(Camera.main.ViewportToWorldPoint(Vector3.zero).x + 1, Camera.main.ViewportToWorldPoint(Vector3.one).x) - 1;
         Vector3 pos = new Vector3(posX, -6, 0);
-        Instantiate(fruit, pos, Quaternion.identity);
+        Instantiate(prefab, pos, Quaternion.identity);
     }
 
     void Start()
     {
-        InvokeRepeating("SpawnFruit", 0f, spawnRate);   
+        InvokeRepeating("Spawn", 0f, spawnRate);
     }
 }
